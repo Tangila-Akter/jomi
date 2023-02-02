5e8b1e931611d3046ac4858f75c58b0538b42129
Tangila-Akter <63112808+Tangila-Akter@users.noreply.github.com>
2023-02-03 02:49:11 +0600

    post

--- resources/views/user/post.blade.php
+++ resources/views/user/post.blade.php
@@ -32,6 +32,67 @@
       </div>
     </div>
   </div>
+  {{-- Modal End --}}
+
+  
+</div>
+<div class="contener-fluid" style="margin: 0% 15% 0% 15%;">
+  <div class="card mb-2 mt-2" style="width: 100%; ">
+    <div class="card-body">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+        <img src="https://picsum.photos/seed/picsum/900/300" alt="" style="width: 35rem; height: 16rem;">
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+        <h1 style="font-size: 30px;font-weight: 400;"> <b>name</b></h1>
+        <p>property details. <br>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. </p>
+        <span>Posted by ... <b></b></span>
+        <div class="mb-6" style="margin-top: 7px;">
+          <button type="submit" class="btn btn-primary" style="background-color: #0d6efd;" >Book Now</button>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+
+  <div class="card mb-2 mt-2" style="width: 100%; ">
+    <div class="card-body">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+        <img src="https://picsum.photos/seed/picsum/900/300" alt="" style="width: 35rem; height: 16rem;">
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+        <h1 style="font-size: 30px;font-weight: 400;"> <b>name</b></h1>
+        <p>property details. <br>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. </p>
+        <span>Posted by ... <b></b></span>
+        <div class="mb-6" style="margin-top: 7px;">
+          <button type="submit" class="btn btn-primary" style="background-color: #0d6efd;" >Book Now</button>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+
+  <div class="card mb-2 mt-2" style="width: 100%; ">
+    <div class="card-body">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+        <img src="https://picsum.photos/seed/picsum/900/300" alt="" style="width: 35rem; height: 16rem;">
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+        <h1 style="font-size: 30px;font-weight: 400;"> <b>name</b></h1>
+        <p>property details. <br>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. </p>
+        <span>Posted by ... <b></b></span>
+        <div class="mb-6" style="margin-top: 7px;">
+          <button type="submit" class="btn btn-primary" style="background-color: #0d6efd;" >Book Now</button>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
 </div>
 @endsection
 @section('js')
